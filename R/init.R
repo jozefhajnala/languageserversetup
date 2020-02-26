773079cdf9f383c99d875069e72cfbef88b8bf52
Jozef <jozef.hajnala@gmail.com>
2020-02-26 14:16:49 +0000

    Add check for SystemRequirements

--- R/init.R
+++ R/init.R
@@ -1,4 +1,16 @@
 .onLoad <- function(libName, pkgName) {
+
+  # Test wheter the system command can run
+  cmd <- get_process_args()[["command"]]
+  res <- system2(cmd, stdout = FALSE, stderr = FALSE)
+  if (res != 0L) {
+    stop(
+      "The command ", sQuote(cmd), " cannot run successfully.\n",
+      "Please make sure the software is available to use the package.\n",
+      if (cmd == "ps") paste("Installing", sQuote("procps"), "might help")
+    )
+  }
+
   options(
     langserver_library = path.expand(file.path("~", "languageserver-library")),
     langserver_processPatt = "languageserver::run",
