c245f2e1b87d25f5c4235f8c00ee84a23203aba7
Jozef Hajnala <jozef.hajnala@gmail.com>
2020-03-16 20:46:00 +0100

    Improve and move CI scripts outside the R package

--- ci/docker_stage.sh
+++ ci/docker_stage.sh
@@ -6,11 +6,11 @@
 
 set -e
 
-container_name=languageserversetup_check
-image_name=index.docker.io/jozefhajnala/rhub:latest
 dir_script=$(dirname $(readlink -f "$0"))
 dir_root=$dir_script/../
 dir_package=$(basename $(dirname $dir_script))
+image_name=index.docker.io/jozefhajnala/rhub:latest
+container_name=${dir_package}_check
 
 docker login --username jozefhajnala --password $DOCKER_LOGIN_TOKEN
 docker pull $image_name
