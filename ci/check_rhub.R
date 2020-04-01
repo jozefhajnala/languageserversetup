4bfcf8cca4d41d5a2aa34c5b1152dc9b17920904
Jozef Hajnala <jozef.hajnala@gmail.com>
2020-04-01 23:11:00 +0100

    Attempt to print test results from rhub artifacts

--- ci/check_rhub.R
+++ ci/check_rhub.R
@@ -37,7 +37,58 @@ res <- do.call(rbind, lapply(statuses, function(thisStatus) {
     stringsAsFactors = FALSE
   )
 }))
+
+message("\n\nTrying to print check results")
 print(res)
+
+
+message("\n\nTrying to print detailed test results")
+message("Determining Candidate URLs and download destinations")
+testCandidates <- do.call(rbind, lapply(
+  cr$urls()$artifacts,
+  function(thisUrl) {
+    testFile <- tools::file_path_sans_ext(dir("./tests/"))
+    data.frame(
+      url = file.path(
+        thisUrl,
+        paste0(unique(sapply(statuses, `[[`, "package"))[[1L]], ".Rcheck"),
+        "tests",
+        c(paste0(testFile, ".Rout"), paste0(testFile, ".Rout.fail"))
+      ),
+      fileName = paste(
+        basename(thisUrl),
+        c(paste0(testFile, ".Rout"), paste0(testFile, ".Rout.fail")),
+        sep = "-"
+      ),
+      stringsAsFactors = FALSE
+    )
+  }
+))
+
+message("Printing Candidate URLs and download destinations: \n")
+print(testCandidates)
+
+message("Attempting do download test logs from rhub: \n")
+testLogs <- apply(
+  testCandidates,
+  1L,
+  function(x) {
+    try(
+      utils::download.file(x["url"], x["fileName"], quiet = TRUE),
+      silent = TRUE
+    )
+  }
+)
+names(testLogs) <- testCandidates[["fileName"]]
+
+message("Attempting do cat test logs from rhub: \n\n")
+invisible(lapply(
+  names(testLogs[testLogs == 0L]),
+  function(x) {
+    message("\n", x)
+    cat(readLines(x), sep = "\n")
+  }
+))
 
 if (any(colSums(res[2L:4L]) > 0)) {
   stop("Some checks with errors, warnings or notes.")
